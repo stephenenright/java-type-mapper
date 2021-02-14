38fd2501040552edff6ec13ccdff392a3de4d6fe
Stephen <11651034+stephenenright@users.noreply.github.com>
2021-02-14 12:58:59 -0600

    Some Javadoc updates

--- src/main/java/com/github/stephenenright/typemapper/TypeMapperConfiguration.java
+++ src/main/java/com/github/stephenenright/typemapper/TypeMapperConfiguration.java
@@ -25,43 +25,66 @@ public interface TypeMapperConfiguration {
 
     /**
      * Add a type converter
+     * @param typeConverter the type converter to add
+     * @param <S>
+     * @param <D>
      */
     public <S, D> void addTypeConverter(TypeConverter<S, D> typeConverter);
 
+
     /**
      * Add a type converter factory
+     * @param factory
+     * @param <S>
+     * @param <D>
      */
     public <S, D> void addTypeConverterFactory(TypeConverterFactory<S, D> factory);
 
     /**
      * Adds a transformer to transform the given property path
+     * @param propertyPath
+     * @param transformer
      */
     public void addPropertyTransformer(String propertyPath, TypePropertyTransformer transformer);
 
+
     /**
      * Returns a <code>TypePropertyTransformer</code> if it exists, otherwise null
+     * @param propertyPath
+     * @return
      */
     public TypePropertyTransformer getPropertyTransformer(String propertyPath);
 
     /**
-     * Adds a post transformer that allows the transformation of the mapped
-     * destination after the mapping is complete
-     * 
+     *  Adds a post transformer that allows the transformation of the mapped
+     *  destination after the mapping is complete
+     * @param transformer
+     * @param <S>
+     * @param <D>
      */
     public <S, D> void setPostTransformer(TypeTransformer<S, D> transformer);
 
     /**
      * Returns the configured post transformer
+     * @param <S>
+     * @param <D>
+     * @return
      */
     public <S, D> TypeTransformer<S, D> getPostTransformer();
 
     /**
      * Returns a type converter for the given source and destination type
+     * @param sourceType
+     * @param destinationType
+     * @param <S>
+     * @param <D>
+     * @return
      */
     public <S, D> TypeConverter<S, D> getTypeConverter(Class<S> sourceType, Class<D> destinationType);
 
     /**
      * Determines if there are include mappings configured
+     * @return
      */
     public boolean hasIncludeMappings();
 
@@ -74,6 +97,7 @@ public interface TypeMapperConfiguration {
 
     /**
      * Determines if there are exclude mappings configured
+     * @return
      */
     public boolean hasExcludeMappings();
 
@@ -86,6 +110,11 @@ public interface TypeMapperConfiguration {
 
     /**
      * Returns if the property should be included in the mapping
+     * @param parentPath
+     * @param propertyPath
+     * @param valueToMap
+     * @param typeInfoRegistry
+     * @return
      */
     public boolean isMappingIncluded(String parentPath, String propertyPath, Object valueToMap,
             TypeInfoRegistry typeInfoRegistry);
